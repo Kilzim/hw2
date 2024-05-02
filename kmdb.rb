dd68041b4b3ae079144e139340762ac72ea12533
Kilian Zimmermann <kilian.zimmermann@kellogg.northwestern.edu>
2024-05-02 18:05:31 +0000

    Commit #2

--- kmdb.rb
+++ kmdb.rb
@@ -98,128 +98,171 @@ Actor.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
-
-# Add the movies to the database
-new_movie = Movie.new
-new_movie["title"] = "Batman Begins"
-new_movie["year"] = "2005"
-new_movie["mpaa_rating"] = "PG-13"
-new_movie["studio"] = "Warner Bros."
-new_movie.save
-
-new_movie2 = Movie.new
-new_movie2["title"] = "The Dark Knight"
-new_movie2["year"] = "2008"
-new_movie2["mpaa_rating"] = "PG-13"
-new_movie2["studio"] = "Warner Bros."
-new_movie2.save
-
-new_movie3 = Movie.new
-new_movie3["title"] = "The Dark Knight Rises"
-new_movie3["year"] = "2012"
-new_movie3["mpaa_rating"] = "PG-13"
-new_movie3["studio"] = "Warner Bros."
-new_movie3.save
-
 # Add the studio to the database
 
 new_studio = Studio.new
 new_studio["name"] = "Warner Bros."
 new_studio.save
 
+# Add the movies to the database
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year"] = "2005"
+new_movie["mpaa_rating"] = "PG-13"
+new_movie["studio"] = new_studio["name"]
+new_movie.save
+
+new_movie2 = Movie.new
+new_movie2["title"] = "The Dark Knight"
+new_movie2["year"] = "2008"
+new_movie2["mpaa_rating"] = "PG-13"
+new_movie2["studio"] = new_studio["name"]
+new_movie2.save
+
+new_movie3 = Movie.new
+new_movie3["title"] = "The Dark Knight Rises"
+new_movie3["year"] = "2012"
+new_movie3["mpaa_rating"] = "PG-13"
+new_movie3["studio"] = new_studio["name"]
+new_movie3.save
+
+# Add the actors to the database
+
+new_actor = Actor.new
+new_actor["name"] = "Christian Bale"
+new_actor.save
+
+new_actor2 = Actor.new
+new_actor2["name"] = "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new
+new_actor3["name"] = "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new
+new_actor4["name"] = "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new
+new_actor5["name"] = "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new
+new_actor6["name"] = "Heath Ledger"
+new_actor6.save
+
+new_actor7 = Actor.new
+new_actor7["name"] = "Aaron Eckhart"
+new_actor7.save
+
+new_actor8 = Actor.new
+new_actor8["name"] = "Maggie Gyllenhaal"
+new_actor8.save
+
+new_actor9 = Actor.new
+new_actor9["name"] = "Tom Hardy"
+new_actor9.save
+
+new_actor10 = Actor.new
+new_actor10["name"] = "Joseph Gordon Levit"
+new_actor10.save
+
+new_actor11 = Actor.new
+new_actor11["name"] = "Anne Hathaway"
+new_actor11.save
+
 # Add the cast to the database
 
 new_cast = Cast.new
-new_cast["movie"] = "Batman Begins"
-new_cast["actor"] = "Christian Bale"
+new_cast["movie"] = new_movie["title"]
+new_cast["actor"] = new_actor["name"]
 new_cast["role"] = "Bruce Wayne"
 new_cast.save
 
 new_cast2 = Cast.new
-new_cast2["movie"] = "Batman Begins"
-new_cast2["actor"] = "Michael Caine"
+new_cast2["movie"] = new_movie["title"]
+new_cast2["actor"] = new_actor2["name"]
 new_cast2["role"] = "Alfred"
 new_cast2.save
 
 new_cast3 = Cast.new
-new_cast3["movie"] = "Batman Begins"
-new_cast3["actor"] = "Liam Neeson"
+new_cast3["movie"] = new_movie["title"]
+new_cast3["actor"] = new_actor3["name"]
 new_cast3["role"] = "Ra's Al Ghul"
 new_cast3.save
 
 new_cast4 = Cast.new
-new_cast4["movie"] = "Batman Begins"
-new_cast4["actor"] = "Katie Holmes"
+new_cast4["movie"] = new_movie["title"]
+new_cast4["actor"] = new_actor4["name"]
 new_cast4["role"] = "Rachel Dawes"
 new_cast4.save
 
 new_cast5 = Cast.new
-new_cast5["movie"] = "Batman Begins"
-new_cast5["actor"] = "Gary Oldman"
+new_cast5["movie"] = new_movie["title"]
+new_cast5["actor"] = new_actor5["name"]
 new_cast5["role"] = "Comissioner Gordon"
 new_cast5.save
 
 new_cast6 = Cast.new
-new_cast6["movie"] = "The Dark Knight"
-new_cast6["actor"] = "Christian Bale"
+new_cast6["movie"] = new_movie2["title"]
+new_cast6["actor"] = new_actor["name"]
 new_cast6["role"] = "Bruce Wayne"
 new_cast6.save
 
 new_cast7 = Cast.new
-new_cast7["movie"] = "The Dark Knight"
-new_cast7["actor"] = "Michael Caine"
+new_cast7["movie"] = new_movie2["title"]
+new_cast7["actor"] = new_actor2["name"]
 new_cast7["role"] = "Alfred"
 new_cast7.save
 
 new_cast8 = Cast.new
-new_cast8["movie"] = "The Dark Knight"
-new_cast8["actor"] = "Heath Ledger"
+new_cast8["movie"] = new_movie2["title"]
+new_cast8["actor"] = new_actor6["name"]
 new_cast8["role"] = "Joker"
 new_cast8.save
 
 new_cast9 = Cast.new
-new_cast9["movie"] = "The Dark Knight"
-new_cast9["actor"] = "Aaron Eckhart"
+new_cast9["movie"] = new_movie2["title"]
+new_cast9["actor"] = new_actor7["name"]
 new_cast9["role"] = "Harvey Dent"
 new_cast9.save
 
 new_cast10 = Cast.new
-new_cast10["movie"] = "The Dark Knight"
-new_cast10["actor"] = "Maggie Gyllenhaal"
+new_cast10["movie"] = new_movie2["title"]
+new_cast10["actor"] = new_actor8["name"]
 new_cast10["role"] = "Rachel Dawes"
 new_cast10.save
 
 new_cast11 = Cast.new
-new_cast11["movie"] = "The Dark Knight Rises"
-new_cast11["actor"] = "Christian Bale"
+new_cast11["movie"] = new_movie3["title"]
+new_cast11["actor"] = new_actor["name"]
 new_cast11["role"] = "Bruce Wayne"
 new_cast11.save
 
 new_cast12 = Cast.new
-new_cast12["movie"] = "The Dark Knight Rises"
-new_cast12["actor"] = "Gary Oldman"
+new_cast12["movie"] = new_movie3["title"]
+new_cast12["actor"] = new_actor5["name"]
 new_cast12["role"] = "Commissioner Gordon"
 new_cast12.save
 
 new_cast13 = Cast.new
-new_cast13["movie"] = "The Dark Knight Rises"
-new_cast13["actor"] = "Tom Hardy"
+new_cast13["movie"] = new_movie3["title"]
+new_cast13["actor"] = new_actor9["name"]
 new_cast13["role"] = "Bane"
 new_cast13.save
 
 new_cast14 = Cast.new
-new_cast14["movie"] = "The Dark Knight Rises"
-new_cast14["actor"] = "Joseph Gordon Levitt"
+new_cast14["movie"] = new_movie3["title"]
+new_cast14["actor"] = new_actor10["name"]
 new_cast14["role"] = "John Blake"
 new_cast14.save
 
 new_cast15 = Cast.new
-new_cast15["movie"] = "The Dark Knight Rises"
-new_cast15["actor"] = "Anne Hathaway"
+new_cast15["movie"] = new_movie3["title"]
+new_cast15["actor"] = new_actor11["name"]
 new_cast15["role"] = "Selina Kyle"
 new_cast15.save
-
-# Add the actors to the database
 
 # Prints a header for the movies output
 puts ""
@@ -237,6 +280,11 @@ for movie in movies
   puts "#{movie.title.ljust(25)} #{movie.year} #{movie.mpaa_rating.ljust(10)} #{movie.studio}"
 end
 
+# studios = Studio.all
+# for studio in studios
+#   puts "#{studio.name}"
+# end
+
 # Prints a header for the cast output
 puts ""
 puts "Top Cast"
@@ -250,6 +298,8 @@ casts = Cast.all
 for cast in casts
   puts "#{cast.movie.ljust(25)} #{cast.actor.ljust(25)} #{cast.role}"
 end
+
+# I googled, how to get a better formating of the output. That is how I came across the .ljust
 
 puts ""
 puts "============================================================================"
